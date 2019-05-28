b6f7c79f8875505946f4b0e97ceba6466172e117
erin johnson <erin.nicole.johnson@gmail.com>
2019-05-28 20:55:38 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(args)
+  i = 0 #start the counter
+  my_collect = []
+  while i < args.length
+    my_collect << yield(args[i]) #push the change into the empty array
+    i +=1
+  end
+  #return the transformation
+  my_collect
+end
