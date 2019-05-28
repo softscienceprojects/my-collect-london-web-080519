1e54991d045271c67ff942b7b8d3c4369d0db1aa
erin johnson <erin.nicole.johnson@gmail.com>
2019-05-28 20:54:07 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(args)
   i = 0 #start the counter
   my_collect = []
   while i < args.length
-    my_collect << yield args[i]
+    my_collect << yield(args[i])
     i +=1
   end
   #return the transformation
